bebcf1ad25825ebdde1e9dea2bf40d4b1ba26d3c
Jared Baur <jaredbaur@fastmail.com>
2021-11-17 23:02:42 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Helpers for booting a Windows VM
---
+++ shell.nix
@@ -0,0 +1,36 @@
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+let
+  image-name = "WindowsVM.img";
+  image-size = "40G";
+  create = writeShellScriptBin "create" ''
+    qemu-img create -f qcow2 ${image-name} ${image-size}
+  '';
+  boot = writeShellScriptBin "boot" ''
+    exec qemu-system-x86_64 -enable-kvm \
+      -cpu host \
+      -smp sockets=1,cores=4,threads=1 \
+      -drive file=${image-name},if=virtio \
+      -net nic -net user,hostname=windowsvm \
+      -m 2G \
+      -monitor stdio \
+      -name "Windows" \
+      -usb \
+      "$@"
+  '';
+  init = symlinkJoin {
+    name = "windoze";
+    paths = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/boot \
+        --add-flags "-boot d -drive file=WINDOWS.iso,media=cdrom -drive file=DRIVER.iso,media=cdrom"
+    '';
+  };
+in
+mkShell {
+  buildInputs = [
+    boot
+    create
+    init
+  ];
+}
